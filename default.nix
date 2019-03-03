537ced695038150659c920cc03ec59d58c701102
John Wiegley <johnw@newartisans.com>
2019-03-03 14:56:05 -0800

    Simplify default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,4 @@
 { compiler    ? "ghc822"
-, doBenchmark ? false
-, doTracing   ? false
-, doStrict    ? false
 , rev         ? "d1ae60cbad7a49874310de91cd17708b042400c8"
 , sha256      ? "0a1w4702jlycg2ab87m7n8frjjngf0cis40lyxm3vdwn7p4fxikz"
 , pkgs        ? import (builtins.fetchTarball {
@@ -19,11 +16,13 @@ let haskellPackages = pkgs.haskell.packages.${compiler};
 in haskellPackages.developPackage {
   root = ./.;
 
+  overrides = self: super: {
+  };
+
   source-overrides = {
   };
 
   modifier = drv: pkgs.haskell.lib.overrideCabal drv (attrs: {
-    inherit doBenchmark;
   });
 
   inherit returnShellEnv;
